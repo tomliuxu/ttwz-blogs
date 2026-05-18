dfa0fa9a5b7326e5b11ef09c56ba54258f1f2b3e
ttwzlx <1874063842@qq.com>
2026-05-18 09:39:07 +0800

    fix: base

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,18 +1,18 @@
-const head = require('./config/head')
-const plugins = require('./plugins')
-const themeConfig = require('./config/themeConfig')
+const head = require("./config/head");
+const plugins = require("./plugins");
+const themeConfig = require("./config/themeConfig");
 
 module.exports = {
-  dest: 'blog',
-  theme: 'reco',
-  title: '铁头娃子的小世界',
-  description: '',
-  base: '/ttwzlx-blogs/', // 部署的根路径
+  dest: "blog",
+  theme: "reco",
+  title: "铁头娃子的小世界",
+  description: "",
+  base: "/ttwz-blogs/", // 部署的根路径
   markdown: {
-    lineNumbers: true
+    lineNumbers: true,
   },
-  dest: './dist',
+  dest: "./dist",
   head,
   plugins,
-  themeConfig
-}
+  themeConfig,
+};
